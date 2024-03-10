fca1bda6bdc5479a9e30b633332ce7c0d64dc7f4
Rabih Andary <rabih@cigotracker.com>
2024-03-10 09:20:19 +0200

    modified customer's reservations' logic

--- app/(app)/(dashboard)/customer/list/index.ts
+++ app/(app)/(dashboard)/customer/list/index.ts
@@ -3,21 +3,34 @@ import {
   where,
   collection,
   getFirestore,
+  getDocs,
   getCountFromServer,
 } from 'firebase/firestore';
 import { getAuthenticatedAppForUser } from '@/firebase/auth';
-import { SPOTS_COLLECTION, SESSION_EXPIRED } from '@/constants';
+import { SPOTS_COLLECTION, RESERVATIONS_COLLECTION, SESSION_EXPIRED } from '@/constants';
 
 export const handleCountReservations = async (id: string) => {
   const app = await getAuthenticatedAppForUser();
   if (!app) throw new Error(SESSION_EXPIRED);
 
-  const q = query(
+  // Fetching the customer's spots...
+  var spotIds: string[] = []
+  const spotsQuery = query(
     collection(getFirestore(app), SPOTS_COLLECTION),
     where('customerId', '==', id),
   );
+  const spotsSnapshot = await getDocs(spotsQuery)
+  spotsSnapshot.forEach((doc: any) => {
+    spotIds.push(doc.data().id)
+  })
 
-  const snapshot = await getCountFromServer(q);
+  // Counting the total number of reservations for all spots...
+  const reservationsQuery = query(
+    collection(getFirestore(app), RESERVATIONS_COLLECTION),
+    where('spotId', 'in', spotIds)
+  )
+  const reservationsSnapshot = await getCountFromServer(reservationsQuery);
+  
 
-  return snapshot.data().count;
+  return reservationsSnapshot.data().count;
 };
