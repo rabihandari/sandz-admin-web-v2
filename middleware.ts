574a5af418a8fdf2a310da9605dfdd7c2f827a8d
Rabih Andari <rabih.n.andari@outlook.com>
2025-01-06 05:28:37 +0200

    Unrestricted the privacy policy from anonymous access

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,19 @@
 import { NextRequest, NextResponse } from 'next/server';
 
+const unrestrictedPages = [
+  "/privacypolicy"
+]
+
 export default async function middleware(request: NextRequest) {
   const session = request.cookies.get('session');
 
   const isOnAuth = request.nextUrl.pathname.startsWith('/login');
 
   if (!session) {
+    if (!isRestricted(request)) {
+      return NextResponse.next();
+    }
+
     if (isOnAuth) return NextResponse.next();
     return NextResponse.redirect(new URL('/login', request.url));
   }
@@ -15,6 +23,15 @@ export default async function middleware(request: NextRequest) {
   }
 
   return NextResponse.next();
+}
+
+function isRestricted(request: NextRequest): Boolean {
+  for (const unrestrictedPage of unrestrictedPages) {
+    if (request.nextUrl.pathname.startsWith(unrestrictedPage)) {
+      return false; 
+    }
+  }
+  return true
 }
 
 export const config = {
